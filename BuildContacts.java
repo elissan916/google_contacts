462ead9e2e24ef43700e0319673fcef2cb530614
Elissa Newman <Elissa Newman>
2017-03-21 15:33:54 -0700

    fixes formatting

--- BuildContacts.java
+++ BuildContacts.java
@@ -1,6 +1,7 @@
 package contacts;
 
 
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -106,38 +107,38 @@ public class BuildContacts {
 				System.err.println("IO Exception caught while reading columns: " + e.getMessage());	
 			}
 			
-			if (i != 0)
-			{
+			if (i != 0) {
 				ContactEntry contact = new ContactEntry();
 				
 				
-				  Name name = new Name();
-				  final String NO_YOMI = null;
+				Name name = new Name();
+				final String NO_YOMI = null;
 				
 				  //Set contact's name
-				  name.setFullName(new FullName(data[0], NO_YOMI));
-				  name.setGivenName(new GivenName(data[0].substring(0, data[0].indexOf(' ')), NO_YOMI));
-				  name.setFamilyName(new FamilyName(data[0].substring(data[0].indexOf(' ')+1,data[0].length()), NO_YOMI));
-				  contact.setName(name);
+				name.setFullName(new FullName(data[0], NO_YOMI));
+				name.setGivenName(new GivenName(data[0].substring(0, data[0].indexOf(' ')), NO_YOMI));
+				name.setFamilyName(new FamilyName(data[0].substring(data[0].indexOf(' ')+1,data[0].length()), NO_YOMI));
+				contact.setName(name);
 				  
-				  // Set contact's e-mail address
-				  Email primaryMail = new Email();
-				  primaryMail.setAddress(data[1]);
-				  primaryMail.setRel("http://schemas.google.com/g/2005#work");
-				  primaryMail.setPrimary(true);
-				  contact.addEmailAddress(primaryMail);
+				// Set contact's e-mail address
+				Email primaryMail = new Email();
+				primaryMail.setAddress(data[1]);
+				primaryMail.setRel("http://schemas.google.com/g/2005#work");
+				primaryMail.setPrimary(true);
+				contact.addEmailAddress(primaryMail);
 				  
-				  // Set contact's phone number
-				  PhoneNumber primaryPhoneNumber = new PhoneNumber();
-				  primaryPhoneNumber.setPhoneNumber(data[2]);
-				  primaryPhoneNumber.setRel("http://schemas.google.com/g/2005#work");
-				  primaryPhoneNumber.setPrimary(true);
-				  contact.addPhoneNumber(primaryPhoneNumber);
+				// Set contact's phone number
+				PhoneNumber primaryPhoneNumber = new PhoneNumber();
+				primaryPhoneNumber.setPhoneNumber(data[2]);
+				primaryPhoneNumber.setRel("http://schemas.google.com/g/2005#work");
+				primaryPhoneNumber.setPrimary(true);
+				contact.addPhoneNumber(primaryPhoneNumber);
 				  
-				  String s = new String () + new Integer(i-1).toString();
-				  contact.setId(s);			    
-				 //Put contact into data structure 
-				 contacts.put(new Integer(i-1), contact);
+				String s = new String () + new Integer(i-1).toString();
+				contact.setId(s);			    
+			
+				//Put contact into data structure 
+				contacts.put(new Integer(i-1), contact);
 			
 			    
 			}
@@ -157,104 +158,100 @@ public class BuildContacts {
 		//List of the threads the batch upload tasks will be run in so we can join them
 		List<Thread> threads = new ArrayList<Thread>();
 
-			//Make the number of threads
-	         for(int i = 0; i < CONCURRENCY_LEVEL; i++) {
+		//Make the number of threads
+	    for(int i = 0; i < CONCURRENCY_LEVEL; i++) {
 	        
-	        	 	//Inner class that the Thread will run
-					class BatchUploadTask implements Runnable {
-	                	 int batchNumber;
+	        //Inner class that the Thread will run
+			class BatchUploadTask implements Runnable {
+	            int batchNumber;
 	                	 
-	                	 //The batchNumber is the number of the i - times 100 plus j is the batch id
-	                	 BatchUploadTask(int batchNumber){ 
-	                		 this.batchNumber = batchNumber;
-	                	 }
+	            //The batchNumber is the number of the i - times 100 plus j is the batch id
+	            BatchUploadTask(int batchNumber){ 
+	                this.batchNumber = batchNumber;
+	            }
 	                	 
-	                	 public void run() {
-	                		 ContactFeed requestFeed = new ContactFeed();
-	                		 ContactsService service = new ContactsService("ContactsExample" + batchNumber);
-	                		 ContactFeed responseFeed;
+	            public void run() {
+	            	ContactFeed requestFeed = new ContactFeed();
+	                ContactsService service = new ContactsService("ContactsExample" + batchNumber);
+	                ContactFeed responseFeed;
 	                		 
-	                 		 try{
-	                 			 //set the credentials of the service
-	                		     service.setUserCredentials("test.exercise5@test.cloud8labs.com","3x3rc1s3");
-	                		 }
-	                		 catch(AuthenticationException e){
-	                		     System.err.println("Authentication Exception caught: " +e.getMessage());
-	                		 }
+	                try{
+	                	//set the credentials of the service
+	                	service.setUserCredentials("test.exercise5@test.cloud8labs.com","3x3rc1s3");
+	                }
+	                catch(AuthenticationException e){
+	                	System.err.println("Authentication Exception caught: " +e.getMessage());
+	                }
 	                		 
-	                    	 for (int j= 0; j < 100; j++)
-	                    	 {
-	                    	     // System.out.printf("%d\n",(batchNumber *100)+ j );
+	               	for (int j= 0; j < 100; j++) {
+	                	// System.out.printf("%d\n",(batchNumber *100)+ j );
 	                    		
-	                    		 //Get the entry from the data structure by the batch id number
-	                    		 ContactEntry entry = contacts.get(new Integer(j + (batchNumber * 100)));
+	                    //Get the entry from the data structure by the batch id number
+	                    ContactEntry entry = contacts.get(new Integer(j + (batchNumber * 100)));
 	                    		 
-	                    		 //add entry to batch create
-	                    		 BatchUtils.setBatchId(entry, "create");
-	                    		 BatchUtils.setBatchOperationType(entry, BatchOperationType.INSERT);
+	                    //add entry to batch create
+	                    BatchUtils.setBatchId(entry, "create");
+	                    BatchUtils.setBatchOperationType(entry, BatchOperationType.INSERT);
 	                    	
-	                    		 //add entry to request Feed
-	                    		 requestFeed.getEntries().add(entry);
+	                    //add entry to request Feed
+	                   	requestFeed.getEntries().add(entry);
 	                    		  
-	                    	 }
+	                }
 							
-	                        try {
+	                try {
 							
-	                        	while(true){	
-	                        		//send the batch requests to the server
-	                        		responseFeed = service.batch(new URL("https://www.google.com/m8/feeds/contacts/default/full/batch"),
-	                        				requestFeed);
+	                	while(true){	
+	                    	//send the batch requests to the server
+	                        responseFeed = service.batch(new URL("https://www.google.com/m8/feeds/contacts/default/full/batch"), requestFeed);
 								
-	                        		boolean retry = false; 
+	                        boolean retry = false; 
 	                        	
-	                        		//For each entry in the responseFeed
-	                        		for (ContactEntry entry : responseFeed.getEntries()) {
-	                        			BatchStatus status = BatchUtils.getBatchStatus(entry);
+	                        //For each entry in the responseFeed
+	                        for (ContactEntry entry : responseFeed.getEntries()) {
+	                        	BatchStatus status = BatchUtils.getBatchStatus(entry);
 			                    	
-	                        			//If there is a temporary problem, retry
-	                        			if (status.getCode() == 503)
-	                        				retry = true;
+	                            //If there is a temporary problem, retry
+	                        		if (status.getCode() == 503)
+	                        			retry = true;
 	                        			
-	                        		}
+	                        }
 		                    	
-	                        		//If retry, let the loop go again, otherwise exit loop
-	                        		if (retry)
-	                        			continue;
-	                        		else
-	                        			return;
-	                        	}
+	                        //If retry, let the loop go again, otherwise exit loop
+	                        if (retry)
+	                        	continue;
+	                        else
+	                        	return;
+	                	}
 		                    	
 		                    	
-							} catch (BatchInterruptedException e) {
-								System.err.println("Batch Interrupted Exception Caught in BatchUploadTask: " + e.getMessage());
-							} catch (MalformedURLException e) {
-								System.err.println("Malformed URL Exception caught in BatchUploadTask: " +e.getMessage() );
-							} catch (IOException e) {
-								System.err.println("IOException caught in BatchUploadTask: " + e.getMessage());
-							} catch (ServiceException e) {
-								System.err.println("ServiceException caught in BatchUploadTask: " + e.getMessage());
-							}
+					} catch (BatchInterruptedException e) {
+						System.err.println("Batch Interrupted Exception Caught in BatchUploadTask: " + e.getMessage());
+					} catch (MalformedURLException e) {
+						System.err.println("Malformed URL Exception caught in BatchUploadTask: " +e.getMessage() );
+					} catch (IOException e) {
+						System.err.println("IOException caught in BatchUploadTask: " + e.getMessage());
+					} catch (ServiceException e) {
+						System.err.println("ServiceException caught in BatchUploadTask: " + e.getMessage());
+					}
 
 
-
-	                     }
-	                 }
+				}
+			}
 	            
-				    Thread t = new Thread(new BatchUploadTask(i));
-				    t.start();
-				    threads.add(t);
+			Thread t = new Thread(new BatchUploadTask(i));
+			t.start();
+			threads.add(t);
 
-	         }
+		}
 
 	         
-	         for (int i = 0; i < threads.size(); i++)
-	         {
-	             try {
-					((Thread)threads.get(i)).join(1000000);
-				} catch (InterruptedException e) {
-					System.err.println("Caught Interrupted Exception while joining threads: " + e.getMessage());
-				}
-	         }
+	    for (int i = 0; i < threads.size(); i++) {
+	    	try {
+				((Thread)threads.get(i)).join(1000000);
+			} catch (InterruptedException e) {
+				System.err.println("Caught Interrupted Exception while joining threads: " + e.getMessage());
+			}
+	    }
 		
 	}
 	
